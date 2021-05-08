ddd94e4d20de18748bba0cceece86ded321be56f
Wellington brito <wellingtonbrito@MacBook-Air-de-Wellington.local>
2021-05-08 19:30:10 -0300

    home

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import 'react-native-gesture-handler';
 import * as Camera from "expo-camera";
 import React from "react";
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
 import { LoadingView } from "./src/LoadingView";
 import { ModelView } from "./src/ModelView";
 import { useTensorFlowLoaded } from "./src/useTensorFlow";
 import PickModelView from "./src/PickModelView";
+import { Home } from "./src/Home";
 
 export default function App() {
   const isLoaded = useTensorFlowLoaded();
@@ -12,6 +15,7 @@ export default function App() {
   const [hasPermission, setHasPermission] = React.useState(null);
   const [type, setType] = React.useState(Camera.Constants.Type.back);
 
+  const Stack = createStackNavigator();
 
   React.useEffect(() => {
     (async () => {
@@ -27,6 +31,16 @@ export default function App() {
     return <LoadingView>Loading TensorFlow</LoadingView>;
   }
 
-  // return <ModelView />;
-  return <PickModelView/>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen options={{headerShown: false}} name="Home" component={Home} />
+        <Stack.Screen name="ModelCam" component={ModelView} />
+        <Stack.Screen name="ModelPhoto" component={PickModelView} />
+
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  )
 }
---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react"
+import { View, Image, Button, Text } from "react-native";
+
+
+
+
+export const Home = ({ navigation }) => {
+
+    return (
+        <View style={{ width: '100%', height: '100%', backgroundColor: 'white' }}>
+
+            <Image source={require("../assets/kasco-logo.png")} style={{ width: 400, height: 400, alignSelf: 'center' }} resizeMode="contain" />
+            <Text style={{ fontWeight: 'bold', alignSelf: 'center', marginHorizontal: 10, fontSize: 20 }}>Bem vindo ao teste de Inteligência Artificial, selecione um dos tipos abaixo</Text>
+
+            <View style={{ backgroundColor: '#4aafff', width: '90%', alignSelf: 'center', marginTop: 80, borderRadius: 4 }}>
+                <Button
+                    color="white"
+                    title="Foto"
+                    onPress={() => console.log("oi")}
+                />
+            </View>
+            <View style={{ backgroundColor: '#800000', width: '90%', alignSelf: 'center', marginTop: 40, borderRadius: 4 }}>
+                <Button
+                    color="white"
+                    title="Câmera"
+                    onPress={() => console.log("oi")}
+                />
+            </View>
+        </View>
+    )
+}
